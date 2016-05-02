8c19598198640339c0854a5e3070f1b29375db1a
Madhu <madhuvanmahesh@gmail.com>
2016-05-01 17:38:21 -0700

    fix: Add autocomplete feature and retrieve API data using AJAX call.
    Delete previous methodology of retrieving data from the API through the collection.

--- js/views/foodList.js
+++ js/views/foodList.js
@@ -3,44 +3,99 @@ var app = app || {};
 
 (function() {
 
-    app.AppView = Backbone.View.extend({
+app.AppView = Backbone.View.extend({
 
-    template: "<input type='text' placeholder='search'> \
-               <button>Search</button> \
-               <ul id='food-list'></ul>",
+    el: '#main',
+    initialize: function() {
+        var fooditem;
+        var records;
+        app.foodCollection.on('add', this.addRecords, this);
+        app.foodCollection.on('reset', this.addAll, this);
+        app.foodCollection.fetch();
 
-    initialize: function(options) {
-        console.log("inside AppView initialize");
-        var food;
     },
-
-    render: function() {
-        console.log("inside AppView render");
-        this.$el.html(this.template);
-        return this;
-    },
-
     events: {
-        'click button': 'getfoodlist'
+
+        'click #add-food': 'addRecords',
+        'keyup #user-input': 'autosearch'
     },
 
-    getfoodlist: function() {
-        console.log("inside AppView getfoodlist");
-        var title = this.$el.find('input').val();
-        food = new app.FoodList({ title: title });
-        food.fetch({ success: this.renderfood.bind(this) });
+    autosearch: function() {
+
+        $("#user-input").autocomplete({
+            delay: 100,
+            source: function(request, response) {
+                var results = [];
+
+                // Suggest URL
+                var suggestURL = "https://api.nutritionix.com/v1_1/search/%QUERY?results=0:10&fields=item_name,brand_name,item_id,nf_calories&appId=41324021&appKey=b16be109bec67fb1282c4b4559e8666f";
+                suggestURL = suggestURL.replace('%QUERY', request.term);
+
+                // JSONP Request
+                $.ajax({
+                    method: 'GET',
+                    dataType: 'json',
+                    url: suggestURL,
+                    data: JSON.stringify({
+                        item_name: "",
+                        brand_name: "",
+                        id: "",
+                        calories: ""
+
+                    }),
+                    success: function(data) {
+
+                        if (data.hits.length != 0) {
+                            for (i = 0; i < data.hits.length; i++) {
+                                response($.map(data.hits, function(item) {
+
+                                    return {
+                                        value: item.fields.item_name,
+                                        item_name: item.fields.item_name,
+                                        id: item.fields.item_id,
+                                        brand_name: item.fields.brand_name,
+                                        calories: item.fields.nf_calories
+
+                                    }
+                                }));
+
+                            }
+                        } else {
+                            response([]);
+                        }
+
+
+                    },
+                    error: function() {
+                        alert("Oops, something went wrong!");
+                    },
+
+                })
+
+            },
+            select: function(event, ui) {
+
+                records = ui.item;
+
+
+            }
+
+        });
 
     },
+    /* End of Autocomplete search */
+    addAll: function() {
 
-    renderfood: function(movies) {
-        console.log("inside AppView renderfood");
-        this.$el.find('#food-list').empty();
-        var foodview;
-        for (var n in food.models) {
-            foodview = new app.FoodItem({ model: food.models[n] });
-            this.$el.find('#food-list').append(foodview.render().el);
-        }
+        app.foodCollection.each(this.addRecords, this);
+
+    },
+    addRecords: function() {
+
+        var view = new app.FoodRecords({ model: records });
+        $('#foodRecords').append(view.render().el);
+
     }
+});
 
 });
 })();
