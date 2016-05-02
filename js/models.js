895dc61e4dcbc81c86537d91e2b04e19bc98c2ed
Madhu <madhuvanmahesh@gmail.com>
2016-05-01 17:34:34 -0700

    fix: Modify and update the defaults properties

--- js/models.js
+++ js/models.js
@@ -3,13 +3,13 @@ var app = app || {};
 (function () {
     'use strict';
     app.FoodItem = Backbone.Model.extend({
-        // Default attributes for the food items
         defaults: {
-        item_name: '',
-        brand_name: '',
-        calories: '',
-        item_id: ''
-      },
+            item_name: '',
+            brand_name: '',
+            calories: '',
+            id:''
+
+        },
 
     });
 })();
