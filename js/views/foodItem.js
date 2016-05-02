6ede933339a826ba09cecffbdfd53bc450903d1e
Madhu <madhuvanmahesh@gmail.com>
2016-05-01 17:43:06 -0700

    fix: Add data to records and collections
    Modify previous methodology by displaying data with a delete button and adding it to collections. Also, add a delete button along with delete functionality.

--- js/views/foodItem.js
+++ js/views/foodItem.js
@@ -1,16 +1,41 @@
-/*global Backbone */
+/* global Backbone */
 var app = app || {};
 
 (function() {
     'use strict';
-    app.FoodItem = Backbone.View.extend({
-        initialize: function(options) {
-            console.log("inside footitem");
-        },
+    app.FoodRecords = Backbone.View.extend({
+        tagName: 'li',
+        template: '<button>Delete</button>',
 
         render: function() {
-            this.$el.html(this.model.attributes.fields.item_name + " , " + this.model.attributes.fields.brand_name + " , " + this.model.attributes.fields.nf_calories);
+            this.$el.html(this.model.item_name);
             return this;
+        },
+
+        initialize: function(options) {
+            if (options.model)
+                this.model = options.model;
+            app.foodCollection.create(this.newAttributes());
+            this.model.on('destroy', this.remove, this);
+
+        },
+
+        events: {
+            'click button': 'destroy'
+        },
+
+        destroy: function() {
+            this.model.destroy();
+        },
+
+        newAttributes: function() {
+            return {
+                item_name: this.model.item_name,
+                brand_name: this.model.brand_name,
+                calories: this.model.nf_calories,
+                id: this.model.item_id
+
+            }
         }
 
     });
